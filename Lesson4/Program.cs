1d595b67cc5da528007293d666bd52ee476c3bcd
vvg88 <grishaninv.v@gmail.com>
2017-03-31 23:13:34 +0300

    Lesson4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            string str = Console.ReadLine();
+
+            StringHelper strHelper = new StringHelper(str);
+            //strHelper.InputString = str;
+
+            Console.WriteLine(strHelper.IsPolyndrome() ? "Является палиндромом" : "Не является палиндромом");
+            Console.WriteLine(string.Format("Количество слов: {0}", strHelper.GetCountWords()));
+            Console.WriteLine($"Перевернутая строка: {strHelper.GetInverseString()}");
+
+            Console.ReadLine();
+            /*try
+            {
+                Array a;
+                int[,] intArr = new int[3, 3];
+                a = intArr;
+                (a as IList)[0] = 1000;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.ReadLine();
+            }*/
+        }
+    }
+    
+}
