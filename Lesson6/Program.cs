a627a8fa2379f52a3be2bb81a240b2e19e3ca830
vvg88 <grishaninv.v@gmail.com>
2017-04-15 10:50:56 +0300

    Lesson6 added

---
+++ Lesson6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
